9899ee7165ff3b2d7d7addd22ad1143384463fc4
Ariana Ledezma <aeledezma72@gmail.com>
2024-05-07 20:22:26 -0700

    Updated phonetic audio button

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  if (!props.phonetic) {
-    return null;
-  }
-
   return (
     <div className="Phonetic">
       <audio controls>
