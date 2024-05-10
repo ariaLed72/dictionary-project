2e2d59e7c88217cbeb998ed65aef89503a87bc40
Ariana Ledezma <aeledezma72@gmail.com>
2024-05-10 01:24:28 -0700

    Finalized project design and details

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <br />
       <em>{props.phonetic.text}</em>
     </div>
   );
